e3e6057486acb1baf17a929ab22d84b7ceac93c5
PeetGaming <110664522+JacobIkke@users.noreply.github.com>
2023-04-14 13:58:31 +0200

    Update LED Voltage Drop Calculator.py

--- LED Voltage Drop Calculator.py
+++ LED Voltage Drop Calculator.py
@@ -18,7 +18,6 @@ led_power = led_voltage * (led_current / 1000) # Power consumption of the led
 
 # Results
 print("")
-#print(f"For a {color} LED with a {supply_voltage}V power supply and {led_current}mA current, use a {round(resistor_value)} ohm resistor.")
 print(f"For {num_leds} x {led_voltage_in}V LED(s) with a {supply_voltage}V power supply and the desired {led_current}mA current, you need to use a {round(resistor_value)} ohm resistor.")
 print(f"The resistor will dissipate {round(resistor_power, 3)} mW of power.")
 print(f"The voltage drop across the resistor will be {round(supply_voltage - led_voltage, 2)}V.")
